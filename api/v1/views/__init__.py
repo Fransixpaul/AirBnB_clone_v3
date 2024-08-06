882e4d4a920e6a4c662b027f227a1a28329de643
fransix <kayininse@gmail.com>
2024-08-06 23:11:28 +0100

    Status of your API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Create a flask app"""
+"""
+create Flask app and register the blue print app_views to flask instance app/
+"""
 from flask import Blueprint
 
-app.view = Blueprint('app_views', __name__, url_prefix='/api.v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
