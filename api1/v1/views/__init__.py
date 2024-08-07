2c8f6dbc04aec65c597f09813d4b84ea5ded2088
francis <krypton@gmail.com>
2024-08-07 01:28:44 +0100

    final test

--- api1/v1/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-create Flask app and register the blue print app_views to flask instance app/
-"""
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
